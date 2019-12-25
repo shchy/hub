09fbb3976c14f6c681fc397e69db3725e081f3b4
shch <shch@gmail.com>
2019-12-25 23:14:18 +0900

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mm/ui/routes/app_router.dart';
-import 'package:mm/ui/routes/routes.dart';
+import 'package:mm/ui/app.dart';
 
 void main() => runApp(App());
-
-class App extends StatefulWidget {
-  @override
-  State createState(){
-    return AppState();
-  }
-}
-
-class AppState extends State<App> {  
-  AppState(){
-    Routes.configure(AppRouter.router);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      onGenerateRoute: AppRouter.router.generator,
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
 
 
