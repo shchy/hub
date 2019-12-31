0d0ad91e62163f5a37268ac8c02b397a956a61c5
shch <shch@gmail.com>
2019-12-31 20:25:55 +0900

    save

---
+++ lib/screens/project/project_list_content.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mm/bloc/project_bloc.dart';
+import 'package:mm/models/project.dart';
+
+class ProjectListContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProjectBloc, Iterable<Project>>(
+      builder: (context, projects) => ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          var project = projects.elementAt(index);
+          return ListTile(
+            onTap: () => print('tap ${project.name}'),
+            leading: Icon(Icons.hotel),
+            subtitle: Text('sub'),
+            title: Text(project.name),
+            trailing: Text(project.name),
+          );
+        },
+        itemCount: projects.length,
+      ),
+    );
+  }
+}
---
+++ lib/screens/project/project_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:mm/components/menu.dart';
+import 'package:mm/screens/project/project_list_content.dart';
+
+class ProjectListView extends StatelessWidget {
+  ProjectListView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('test'),
+        elevation: 0.0,
+      ),
+      drawer: GestureDetector(
+        child: Drawer(child: SideMenu()),
+      ),
+      body: ProjectListContent(),
+    );
+  }
+}
